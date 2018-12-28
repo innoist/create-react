bac6c0d9b0238640104b1a1338a6195217b9f312
Syed Usman <syedusman.1@gmail.com>
2018-12-28 17:16:03 +1100

    contianers and other folders

--- src/App.js
+++ src/App.js
@@ -1,48 +1,13 @@
 import React, { Component } from "react";
-import { connect } from "react-redux";
-import { simpleAction } from "./actions/simpleAction";
-import ChildApp from "./child";
-import logo from "./images/logo.svg";
+import SimplePage from "./containers/simplePage";
 import "./styles/App.css";
 
-class App extends Component {
-  simpleAction = event => {
-    this.props.simpleAction();
-  };
+export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit asd asd <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        <button onClick={this.simpleAction}> count redux action</button>
-        <ChildApp simple={this.props.simple} />
+        <SimplePage />
       </div>
     );
   }
 }
-const mapStateToProps = state => {
-  const simple = state.simpleReducer.result;
-  return {
-    simple: simple
-  };
-};
-
-const mapDispatchToProps = dispatch => ({
-  simpleAction: () => dispatch(simpleAction())
-});
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(App);
